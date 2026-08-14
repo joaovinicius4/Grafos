81e5a609686db659d7ab57e665ed4bddcecbded6
joaovinicius4 <joaoruas111@gmail.com>
2026-08-14 11:12:53 -0300

    add estrutura do projeto

---
+++ Rotas.py
@@ -0,0 +1,161 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+import networkx as nx
+import matplotlib.pyplot as plt
+
+load_dotenv()
+
+MINHA_CHAVE = os.getenv("MINHA_CHAVE")
+
+client = genai.Client(api_key=MINHA_CHAVE)
+
+
+
+cidades = [
+    "São Paulo",
+    "Rio de Janeiro",
+    "Buenos Aires",
+    "Santiago",
+    "Lima",
+    "Bogotá",
+    "Cidade do México",
+    "Nova York",
+    "Toronto",
+    "Los Angeles",
+    "Londres",
+    "Paris",
+    "Madrid",
+    "Roma",
+    "Dubai",
+    "Cairo",
+    "Tóquio",
+    "Pequim",
+    "Sydney",
+    "Joanesburgo"
+]
+
+
+prompt = f"""
+Tenho estas 20 cidades:
+
+{chr(10).join(cidades)}
+
+Liste as rotas aéreas comerciais DIRETAS entre essas cidades.
+
+Considere somente voos diretos entre duas das cidades da lista.
+
+Retorne SOMENTE as rotas, uma por linha, exatamente neste formato:
+
+São Paulo;Rio de Janeiro
+São Paulo;Buenos Aires
+Londres;Paris
+
+Não escreva explicações.
+Não use números.
+Não coloque marcadores.
+Não coloque código.
+"""
+
+resposta = client.models.generate_content(
+    model="models/gemini-3.1-flash-lite",
+     contents=prompt
+)
+
+texto_rotas = resposta.text
+
+
+with open("rotas.txt", "w", encoding="utf-8") as arquivo:
+    arquivo.write(texto_rotas)
+
+print("Rotas recebidas do Gemini.")
+print("Rotas salvas em rotas.txt")
+
+N = len(cidades)
+
+matriz = [[0 for _ in range(N)] for _ in range(N)]
+
+for linha in texto_rotas.splitlines():
+
+    linha = linha.strip()
+
+    if ";" not in linha:
+        continue
+
+    origem, destino = linha.split(";", 1)
+
+    origem = origem.strip()
+    destino = destino.strip()
+
+    if origem not in cidades or destino not in cidades:
+        print("Rota ignorada:", linha)
+        continue
+
+    i = cidades.index(origem)
+    j = cidades.index(destino)
+
+    # Grafo não direcionado
+    matriz[i][j] = 1
+    matriz[j][i] = 1
+
+print("CIDADES")
+
+
+for i, cidade in enumerate(cidades):
+    print(f"{i:2} - {cidade}")
+
+
+
+print("MATRIZ DE ADJACÊNCIA")
+
+
+print("     ", end="")
+
+for i in range(N):
+    print(f"{i:2}", end=" ")
+
+print()
+
+for i in range(N):
+
+    print(f"{i:2}   ", end="")
+
+    for j in range(N):
+        print(f"{matriz[i][j]:2}", end=" ")
+
+    print()
+
+
+# PLOTAR O GRAFICO
+
+G = nx.Graph()
+
+for i in range(len(cidades)):
+    G.add_node(i, label=cidades[i])
+
+for i in range(len(cidades)):
+    for j in range(i + 1, len(cidades)):
+        if matriz[i][j] == 1:
+            G.add_edge(i, j)
+
+pos = nx.spring_layout(G, seed=42)
+
+nx.draw(
+    G,
+    pos,
+    with_labels=True,
+    node_size=1000,
+    font_size=8
+)
+
+labels = {i: cidades[i] for i in range(len(cidades))}
+
+nx.draw_networkx_labels(
+    G,
+    pos,
+    labels=labels,
+    font_size=7
+)
+
+plt.title("Grafo das Rotas Aéreas")
+plt.show()
